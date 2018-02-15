6220cc6b2b07036179d0705c079324531b565588
siddish reddy <Siddish.ibv@gmail.com>
2018-02-15 10:52:25 +0530

    Update and rename bubblesort.py to bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -3,7 +3,7 @@ def bubblesort(list):
         for j in range(i+1,len(list)):
             if list[i]>list[j]:
                 list[i],list[j]=list[j],list[i]
-        print("list:",list,"after iteration ",i)
+        print("list:",list,"after iteration ",i+1)
     print("sorted list:",list)
                 
                 
